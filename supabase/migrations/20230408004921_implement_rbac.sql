7dea2f023f8049f746bada581c999b3d22a999ef
Francis Nimick <fnimick@gmail.com>
2023-06-16 19:39:20 -0400

    Update `update_user_roles` to remove empty groups
    remove group entries from groups object when last role is removed

--- supabase/migrations/20230408004921_implement_rbac.sql
+++ supabase/migrations/20230408004921_implement_rbac.sql
@@ -140,17 +140,18 @@ update auth.users
 set raw_app_meta_data = jsonb_set(
     raw_app_meta_data,
     '{groups}',
-    jsonb_set(
-      coalesce(raw_app_meta_data->'groups', '{}'::jsonb),
-      array [_group_id::text],
-      coalesce(
-        (
-          select jsonb_agg("role")
-          from group_users gu
-          where gu.group_id = _group_id
-            and gu.user_id = _user_id
-        ),
-        '[]'::jsonb
+    jsonb_strip_nulls(
+      jsonb_set(
+        coalesce(raw_app_meta_data->'groups', '{}'::jsonb),
+        array [_group_id::text],
+        coalesce(
+          (select jsonb_agg("role")
+            from group_users gu
+            where gu.group_id = _group_id
+              and gu.user_id = _user_id
+          ),
+          'null'::jsonb
+        )
       )
     )
   )
